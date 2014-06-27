a1dc4225a9a5ad393c9f93e8251910f4c63a294a
kundum <maitreya.kundurthy@emc.com>
2014-06-27 15:34:48 -0500

    ball reset added

--- ball.js
+++ ball.js
@@ -1,13 +1,6 @@
 function Ball(){
     Entity.call(this)
-    this.width = 20 
-    this.height = 20
-    
-    this.xVel = 2
-    this.yVel = 3
-
-    this.x = game.width / 2 - this.width
-    this.y = game.height/ 2 - this.height
+    this.reset()
 }
 
 Ball.prototype = Object.create(Entity.prototype)
@@ -15,12 +8,25 @@ Ball.prototype.constructor = Ball
 
 //override the update funtion 
 
+Ball.prototype.reset = function(){
+    this.width = 20 
+    this.height = 20
+    
+    this.xVel = 2 *(Math.random()>0.5?-1:1)
+    this.yVel = 3 *(Math.random()>0.5?-1:1)
+
+    this.x = game.width / 2 - this.width
+    this.y = game.height/ 2 - this.height
+}
+
 Ball.prototype.update = function(){
     Entity.prototype.update.call(this)
     var hitter 
     
     if (this.x > game.width - this.width || this.x<0){
         this.xVel *=-1
+        this.reset()
+        
     }
     if (this.y > game.height - this.height || this.y<0){
         this.yVel *=-1
